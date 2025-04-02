eef7a7b658b11797abb0939dcab85417a8a96c92
deivisonpsilva <deivisonpereirasilva89@gmail.com>
2025-04-02 01:22:04 +0000

    Inclusão de Dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,7 +18,39 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    printf("Desafio Super Trunfo!\n");
+    char Estado, Código, Cidade;
+    int População, Pontos;
+    float Área, PIB;
+
+    printf("Para definir o Estado, digite uma letra entre A - H");
+    scanf(" %c", &Estado);
+    printf("Estado: %c \n", Estado);
+
+    printf("Para definir o Código da Carta, digite o codigo do seu Estado seguido do numero 01, 02, 03 ou 04 (ex: A03, H01).");
+    scanf("%s", &Código);
+    printf("Codigo: %s \n", Código);
+
+    printf("Digite o nome da cidade:");
+    scanf("%s", Cidade);
+    printf("Cidade: %c \n", Cidade);
+
+    printf("Qual o numero da população da cidade?");
+    scanf("%d", &População);
+    printf("População: %d \n", População);
+
+    printf("Qual a área da sua cidade?");
+    scanf("%f", &Área);
+    printf("Codigo: %f \n", Área);
+
+    printf("Qual o PIB da sua cidade?");
+    scanf("%f", &PIB);
+    printf("PIB: %f \n", PIB);
+
+    printf("Quantos pontos turisticos tem a sua cidade?");
+    scanf("%s", &Pontos);
+    printf("Pontos Turísticos: %s \n", Pontos);
+
+
 
     return 0;
 }
